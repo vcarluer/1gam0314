082482b952d4a03b7c5ed8c6436f6f16738b87c2
Vincent Carluer <vcarluer@gmail.com>
2014-03-25 13:43:24 +0100

    particle in atlas and code!

--- src/aitrinity/src/gamers/associate/Aitrinity.java
+++ src/aitrinity/src/gamers/associate/Aitrinity.java
@@ -1,5 +1,7 @@
 package gamers.associate;
 
+import java.util.ArrayList;
+
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
@@ -9,6 +11,9 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.Texture.TextureFilter;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.ParticleEffect;
+import com.badlogic.gdx.graphics.g2d.ParticleEffectPool;
+import com.badlogic.gdx.graphics.g2d.ParticleEffectPool.PooledEffect;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
@@ -33,6 +38,10 @@ public class Aitrinity implements ApplicationListener {
 	
 	private FreeTypeFontGenerator fontGenerator;
 	
+	private ParticleEffectPool pool;
+	private ArrayList<PooledEffect> effects;
+	
+	
 	@Override
 	public void create() {		
 		float w = Gdx.graphics.getWidth();
@@ -56,15 +65,24 @@ public class Aitrinity implements ApplicationListener {
 		stateTime = 0f;
 		
 		// second method
-		atlas = new TextureAtlas(Gdx.files.internal("data/activate1.atlas"));
-		activateFrames = atlas.getRegions();
+		atlas = new TextureAtlas(Gdx.files.internal("data/pack.atlas"));
+		activateFrames = atlas.findRegions("ia1/activate");
 		
 		activateAnimation = new Animation(0.1f, activateFrames);
 		
 		// font = new BitmapFont(Gdx.files.internal("data/04b03.fnt"), Gdx.files.internal("data/04b03.png"), false);
 		fontGenerator = new FreeTypeFontGenerator(Gdx.files.internal("data/04b03.ttf"));
 		font = fontGenerator.generateFont(20);
-		font.setColor(Color.BLACK);
+		font.setColor(Color.GREEN);
+		
+		effects = new ArrayList<PooledEffect>();
+		ParticleEffect prototype = new ParticleEffect();
+		prototype.load(Gdx.files.internal("data/zerogreen2.p"), atlas);
+		pool = new ParticleEffectPool(prototype, 1, 2);
+		
+		PooledEffect effect = pool.obtain();
+		effects.add(effect);
+		
 		
 //		TextureRegion region = new TextureRegion(texture, 0, 0, 256, 64);
 //		
@@ -82,16 +100,21 @@ public class Aitrinity implements ApplicationListener {
 
 	@Override
 	public void render() {		
-		Gdx.gl.glClearColor(1, 1, 1, 1);
+		Gdx.gl.glClearColor(0, 0, 0, 0);
 		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
 		
 		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
 		// sprite.draw(batch);
-		stateTime += Gdx.graphics.getDeltaTime();
+		float delta = Gdx.graphics.getDeltaTime();
+		stateTime += delta;
 		TextureRegion texture = activateAnimation.getKeyFrame(stateTime, true);
 		batch.draw(texture, -texture.getRegionWidth() / 2f, -texture.getRegionHeight() / 2f);
 		font.draw(batch, "Yop", 0, 0);
+		for(PooledEffect effect : effects) {
+			effect.draw(batch, delta);
+		}
+		
 		batch.end();		
 	}
 
---
+++ tools/pyxel.cmd
@@ -0,0 +1,2 @@
+cd "C:\ga\PyxelEdit0.3.106\"
+PyxelEdit.exe
