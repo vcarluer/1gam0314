fea8ce56f382ae736145f3338d392f8367336e23
Vincent Carluer <vcarluer@gmail.com>
2014-03-25 11:55:46 +0100

    particle editor cmd

---
+++ tools/particle.cmd
@@ -0,0 +1,1 @@
+java -cp ..\..\libgdx\gdx.jar;..\..\libgdx\gdx-natives.jar;..\..\libgdx\gdx-backend-lwjgl.jar;..\..\libgdx\gdx-backend-lwjgl-natives.jar;..\..\libgdx\extensions\gdx-tools\gdx-tools.jar com.badlogic.gdx.tools.particleeditor.ParticleEditor
